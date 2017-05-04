d4c3e4843c645a88329c385a7d8c76da56b7d023
mahjnalisha <mahjnalisha@gmail.com>
2017-05-04 15:38:01 +1000

    write to the file

--- spool/spool.js
+++ spool/spool.js
@@ -1,11 +1,38 @@
 module.exports = function(RED) {
-    function SpoolNode(config) {
-        RED.nodes.createNode(this,config);
+    var fs=require('fs');
+    function SpoolNode(n) {
+        RED.nodes.createNode(this,n);
+        this.filename = n.filename || "";
+        this.loadedScript = '';
+        this.loadedFilename = '';
+        this.loadedbrokerstatus = '';
+
         var node = this;
+        // Read and file when node is initialized,
+
+        if (this.filename !== '') {
+            node.loadedFilename = this.filename;
+
+
+            fs.readFile(this.filename, {encoding: 'utf-8'}, function (err, fileContent) {
+                if (err) {
+                    if (err.code === 'ENOENT') {
+                        node.warn('Could not find file "' + err.path + '". Hint: File path is relative to "' + process.env.PWD + '"');
+                    } else {
+                        node.warn(err);
+                    }
+                } else {
+                    node.loadedScript = fileContent;
+                }
+            });
+        }
+
+
         this.on('input', function(msg) {
-            msg.payload = msg.payload.toLowerCase();
+            fs.appendFileSync(this.filename,msg.payload);
             node.send(msg);
         });
     }
     RED.nodes.registerType("spool",SpoolNode);
+    RED.library.register("functions");
 }
