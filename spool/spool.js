3d4bf14f344632fd0fc673c818d5bcf93ed56074
nita <nitaregmi@gmail.com>
2017-05-10 17:45:47 +1000

    reading from file

--- spool/spool.js
+++ spool/spool.js
@@ -83,6 +83,7 @@ module.exports = function(RED) {
                     }
 
                 }).on('close', function() {
+
                 });
 
                 function deleteSelectedLine(arrfile)
